43fcd551ab85f77b30328acfb0f6a82426fa1f11
Arko Sengupta <arkosengupta9@gmail.com>
2024-03-25 00:46:09 +0530

    Task-2

--- Task-2-Scientific-Calculator/src/App.js
+++ Task-2-Scientific-Calculator/src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Container } from "react-bootstrap";
 import Calculator from "./components/Calculator";
 
-function App() {
+function App(){
   return (
     <div className="App">
       <Container className="d-flex justify-content-center align-items-center h-100">
